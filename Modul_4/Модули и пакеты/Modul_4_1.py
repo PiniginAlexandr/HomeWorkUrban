1eaeb62705d5681a8487b3279efbf3889cb9deb5
PiniginAlexandr <pul9dur2@gmail.com>
2024-12-02 14:53:11 +0300

    По указаниям преподавателя, исправил свою ошибку. Сделал импорт одной функции с другого модуля. С помощью: "from {назв.мод.} import {Назв.ф.} as {Назв.ф.в этом модуле}"

--- Modul_4/Модули и пакеты/Modul_4_1.py
+++ Modul_4/Модули и пакеты/Modul_4_1.py
@@ -1,10 +1,10 @@
-import fake_math as fake_divide
-import true_math as true_divide
+from fake_math import divide as fake_divide
+from true_math import divide as true_divide
 
-result_1 = fake_divide.divide(69, 3)
-result_2 = fake_divide.divide(3, 0)
-result_3 = true_divide.divide(49, 7)
-result_4 = true_divide.divide(15, 0)
+result_1 = fake_divide(69, 3)
+result_2 = fake_divide(3, 0)
+result_3 = true_divide(49, 7)
+result_4 = true_divide(15, 0)
 print(result_1)
 print(result_2)
 print(result_3)
