d3f107e99ca6de824157f8db2f27f9ad904cd24d
Shilk <pul9dur2@gmail.com>
2025-01-05 16:39:31 +0300

    Добавил решение задачи "Файлы в операционной системе."

---
+++ Modul_7/ДЗ(Файлы в операционной системе)/Файлы в операционной системе.py
@@ -0,0 +1,16 @@
+import os
+import time
+
+directory = os.getcwd()
+for root, dirs, files in os.walk(directory):
+    for file in files:
+        file_path = os.path.join(root, file)
+        file_time = os.path.getmtime(file_path)
+        formatted_time = time.strftime('%d.%m.%Y %H:%M:%S', time.localtime(file_time))
+        file_size = os.path.getsize(file_path)
+        parent_dir = os.path.dirname(file_path)
+        print(f'Обнаружен файл: {file};'
+              f'\nПуть: {file_path};'
+              f'\nРазмер: {file_size} байт;'
+              f'\nВремя изменения: {formatted_time};'
+              f'\nРодительская дириктория: {parent_dir}.')
